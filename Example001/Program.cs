342a2303ecefc5b11258fbcef683bf41a9343845
Evgeniy <zadrod93@yandex.ru>
2022-02-07 16:37:27 +0300

    Задача номер 12

---
+++ Example001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int GetRandom()
+{
+    int num = new Random().Next(100, 999);
+    return num;
+}
+
+int randomizer = GetRandom();
+
+Console.WriteLine("Трёхзначное число : " + randomizer);
+
+int result = randomizer % 100 / 10;
+
+Console.WriteLine("Вторая цифра трёхзначного числа = " + result);
