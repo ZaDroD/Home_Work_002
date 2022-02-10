05b2cd06de7026251785461f7404bdf5ace99d2b
Evgeniy <zadrod93@yandex.ru>
2022-02-10 19:03:10 +0300

    Задача номер 14

---
+++ Example003/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти третью цифру числа или сообщить что её нет
+
+int GetRandom()
+{
+    int number = new Random().Next(0, 999);
+    return number;
+}
+int randomizer = GetRandom();
+Console.WriteLine("Случайное число " + randomizer);
+int result = randomizer / 10;
+if (result >= 10)
+{
+    Console.WriteLine(randomizer % 10);
+}
+else
+{
+    Console.WriteLine("Третье число отсутствует");
+}
